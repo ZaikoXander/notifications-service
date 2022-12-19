878d0c9a4294cf78240213d44653b38fc19a93ea
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-12-19 19:40:56 -0300

    Prepare to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
 import { MessagingModule } from '@infra/messaging/messaging.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule, DatabaseModule, MessagingModule],
+  imports: [
+    HttpModule,
+    DatabaseModule,
+    MessagingModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,12 +1,11 @@
 import { SendNotification } from '@app/use-cases/send-notification';
 import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { NotificationsController } from './kafka/controllers/notifications.controller';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot()],
+  imports: [DatabaseModule],
   providers: [KafkaConsumerService, SendNotification],
   controllers: [NotificationsController],
 })
